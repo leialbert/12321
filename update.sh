be9504880637dd7250a5b945c815b024d13a0fc4
Albert Lei <albert.lei@hotmail.com>
2023-06-13 14:16:25 +0800

    update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Changing to the correct directory..."
+# Change to the directory where your git repository is located
+cd /data/v12321
+echo "Activating virtual environment..."
+source env/bin/activate
+# Run git pull
+git pull origin main 
+# Run any migrations and restart the server
+echo "Running migrations..."
+python manage.py migrate
+echo "Restarting server..."
+systemctl restart gunicorn
+echo "Completed."
